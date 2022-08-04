bd34e2cd56859fd56da0add44e23f65bef44cf13
leejiayi <leejiayi615@gmail.com>
2022-08-05 07:01:02 +0800

    more boxes and random colors

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-import $ from "jquery"
-$("body").append('<h2></h2>') //append once only
-  $('h2').text("")// ? score
+import $, { get } from "jquery"
+
   //array of colors in (rgb)
   let count = 0
   let colorcolor = ["rgb(255,0,0)", "rgb(255,165,0)", "rgb(255,255,0)", "rgb(0,100,0)", "rgb(0, 71, 171)", "rgb(195, 177, 225)", "rgb(75, 0, 130)"]
-  let x = []
+  //let x = []
  let inputValue 
- 
+ let randomnum1 = Math.floor(Math.random()*25)
+ const oneSquare = document.getElementById('square2')
+  
 
  //start screen -> show start screen and hide the rest 
 const showStart = () => {
-  location.reload()
-  // $(".start").show("slow");
-  // $(".score").hide();
-  // $(".board").hide(); 
+
+  $(".start").show("slow");
+  $(".score").hide();
+  $(".board").hide(); 
+  count = 0
 };
 
 
@@ -36,7 +38,29 @@ const showScore = () => {
   $(".score").show("slow");
   $(".start").hide();
   $(".board").hide();
+  $(".board1").hide()
+  //scoreBoard()
 };
+
+const countDownTimer = () => {
+  let timeleft = 15;
+  let downloadTimer = setInterval(function(){
+  //if time = 0 or lesser, clear the interval and show "finished"
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+    showScore();
+    //scoreBoard();
+  } else {
+    //or else -> keep showing text of "timeleft"
+    document.getElementById("countdown").innerHTML = timeleft + " sec";
+  }
+  // minus 1 everytime
+  
+  timeleft -= 1;
+  //interval = 1000 milliseconds = 1 second
+}, 1000);
+}
 
 
 const makeSquare = () => {
@@ -47,22 +71,7 @@ const makeSquare = () => {
   //})
   //countdown timer
   // setInterval(function, milleseconds)
-  let timeleft = 3;
-  let downloadTimer = setInterval(function(){
-  //if time = 0 or lesser, clear the interval and show "finished"
-  if(timeleft <= 0){
-    //clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished";
-    showScore();
-  } else {
-    //or else -> keep showing text of "timeleft"
-    document.getElementById("countdown").innerHTML = timeleft + " sec";
-  }
-  // minus 1 everytime
   
-  timeleft -= 1;
-  //interval = 1000 milliseconds = 1 second
-}, 1000);
 document.getElementById("score").innerHTML = "Score " + count;
 
 //get the squares which created in HTML
@@ -97,93 +106,88 @@ document.getElementById("score").innerHTML = "Score " + count;
         }
         //when user click on the box
         $(".square").on("click", () => {
-          clearInterval(downloadTimer);
+          //clearInterval(downloadTimer);
           //get the index of box being clicked
-          const indexClicked = $(event.target).index(this);
-          console.log(indexClicked)
-          
-          
-          //if the index of box being clicked is equal to the 
-          //random number/odd color
-          // const allNumbers = document.getElementsByClassName("square")
-          // for (let j = 0; j < allNumbers.length; j++) {
+          let indexClicked = $(event.target).index(this);
+          console.log(indexClicked + "hi")
           if (indexClicked === randomnumbers) {
             count++
-            
-            console.log(count)
-            //it will keep repeating
-            console.log(indexClicked)
-            console.log(randomnumbers) 
-           
-            //indexClicked.splice(0, indexClicked.length)
             makeSquare()
-            
-          } 
-          else if (indexClicked !== randomnumbers) {
-          return false
-          //alert("oh no")
-          
-          }
-        
+          indexClicked = -1
+          randomnumbers = -2
+          }       
       })
       }
-  
-
-      
       const makeMoreSquares = () => {
         document.getElementById("welcome1").innerHTML = "Welcome " + inputValue
-        let timeleft = 5;
-        let downloadTimer = setInterval(function(){
-        if(timeleft <= 0){
-        document.getElementById("countdown1").innerHTML = "Finished";
-      showScore();
-      } else {
-     document.getElementById("countdown1").innerHTML = timeleft + " sec";
+        let timeleft1 = 15;
+        
+  let downloadTimer1 = setInterval(function(){
+  //if time = 0 or lesser, clear the interval and show "finished"
+  if(timeleft1 <= 0){
+    clearInterval(downloadTimer1);
+    document.getElementById("countdown1").innerHTML = "Finished";
+    showScore();
+    scoreBoard();
+    console.log(scoreBoard)
+  } else {
+    //or else -> keep showing text of "timeleft"
+    document.getElementById("countdown1").innerHTML = timeleft1 + " sec";
   }
-  timeleft -= 1;  
+  // minus 1 everytime
+  timeleft1 -= 1;
+  
+  //interval = 1000 milliseconds = 1 second
 }, 1000);
+
 document.getElementById("score1").innerHTML = "Score " + count;
 const getMoreBoxes = document.getElementsByClassName('square1');
-let randomcolors = colorcolor[Math.floor(Math.random() * colorcolor.length)]
+let randomcolors1 = colorcolor[Math.floor(Math.random() * colorcolor.length)]
     
-        let oddcolor = randomcolors.split(/[(\)]/)
-        console.log(oddcolor)
-        let changeToInt = oddcolor[1].split(",").map(element => (parseInt(element)))
-        console.log(changeToInt)
-        let randomRgb = 70 + (Math.floor(Math.random() * 100))
-        const rgbCode = "rgb" + "(" + changeToInt[0] + "," + changeToInt[1] + "," + randomRgb + ")"
-        console.log(rgbCode)
-        let randomnumbers = Math.floor(Math.random() * 25)
-         console.log(randomnumbers)
-        for ( let i=0; i< getMoreBoxes.length; i++ ) {
-        // if the random number is not equal to the index of the box -> random color
-        // will be assigned
-          if (getMoreBoxes[i] !== getMoreBoxes[randomnumbers]) {
-            getMoreBoxes[i].style.backgroundColor = randomcolors
-            //if the random number is equal to index of the box -> odd color
-            //will be assigned
-          } else
-          getMoreBoxes[randomnumbers].style.backgroundColor = rgbCode
-        }
         
-        const game = () => {
-          clearInterval(downloadTimer);
-          const indexClicked = $(event.target).index(this);
-          if (indexClicked === randomnumbers) {
-            count++
-            showMedium();
-          } else showScore()
+          function random_bg_color(){
+            var x = Math.floor(Math.random() * 256);
+            var y = Math.floor(Math.random() * 256);
+            var z = Math.floor(Math.random() * 256);
+            var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+            return bgColor;
         }
-        const element = document.getElementsByClassName("square1");
-        element.addEventListener("click", game());
-        //when user click on the box
-        // $(".square").on("click", () => {
-        
-          
-        //   console.log(indexClicked)   
-          
-
+          for (let k = 0; k < getMoreBoxes.length; k++) {
+            getMoreBoxes[k].style.backgroundColor = random_bg_color();
+            console.log(random_bg_color)
+            
+            oneSquare.style.backgroundColor = getMoreBoxes[randomnum1].style.backgroundColor
+            console.log(randomnum1)
+    
+              }
       }
+        $(".square1").on("click", () => {
+          // clearInterval(downloadTimer1);
+          //get the index of box being clicked
+          let indexClicked1 = $(event.target).index(this);
+          console.log(indexClicked1 + "hi")
+          console.log(randomnum1 + "hello")
+          if (indexClicked1 === randomnum1) {
+            count++
+            console.log(count)
+            
+            makeMoreSquares()
+          }       
+      })        
+      
+      
+
+      const scoreBoard = () => {
+        let yourScore = document.getElementById("yourscoreis").innerHTML = "Your score is " + count
+        let tbodyRef = document.getElementById("table").getElementsByTagName("tbody")[0];
+        let newRow = tbodyRef.insertRow();
+        let newCell = newRow.insertCell();
+        let newText = document.createTextNode(inputValue + " : " + count);
+        
+        newCell.appendChild(newText)
+        
+      }
+  
 
   
   
@@ -194,6 +198,7 @@ const main = () => {
     console.log(inputValue);
     $("#input-box").val("");
     showBoard()
+    countDownTimer();
   })
   $("#mediumbutton").on("click", () => {
     inputValue = $("#input-box").val();
@@ -203,11 +208,6 @@ const main = () => {
   })
   $("#gameButton").on("click", showScore)
   $("#restartButton").on("click", showStart)
-//showStart();
-
-//   inputValue = $("#input-box").val();
-//   console.log(inputValue);
-//   $("#input-box").val("");
 
 };
 
